d6e23a0fa733ccbd3ebd9adf02d0c8d9174fc73d
Manuel Santos Souza <manuel.souza.017@ufrn.edu.br>
2024-10-12 12:28:30 -0300

    Creating the XCode project.

---
+++ Pom/Pom/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Pom
+//
+//  Created by Manuel Santos Souza on 12/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
---
+++ Pom/PomTests/PomTests.swift
@@ -0,0 +1,17 @@
+//
+//  PomTests.swift
+//  PomTests
+//
+//  Created by Manuel Santos Souza on 12/10/24.
+//
+
+import Testing
+@testable import Pom
+
+struct PomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
